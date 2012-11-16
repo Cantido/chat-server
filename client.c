85c8d0dbd9aadb149e543779d606ca568c25773d
Robert Richter <rcrvy4@mst.edu>
2012-11-15 18:15:37 -0600

    Moved client reading into a thread to avoid blocking

--- client.c
+++ client.c
@@ -14,17 +14,20 @@
 
 #define SERVER_PORT 9999
 #define HOST_NAME "localhost"
+
+void *read_from_server(void *arg);
  
 int main() {
 	int streamSocket;
+	int tid;
 	
 	struct sockaddr_in serverAddress = { AF_INET, htons(SERVER_PORT) };
 	
 	int serverLength = sizeof(serverAddress);
 	
 	struct hostent *hp;
-	
-	char buf[512];
+
+	char buf[BUFSIZ];
 	int charsRead;
 	
 	hp = gethostbyname(HOST_NAME);
@@ -34,18 +37,33 @@ int main() {
 	streamSocket = socket(AF_INET, SOCK_STREAM, 0);
 	
 	if(connect(streamSocket, (struct sockaddr*) &serverAddress, serverLength) == -1) {
-		perror("Client: connect failed");
+		perror("Client error:");
 		exit(1);
 	}
 	
 	printf("Client has connected to the server.\n");
 	
-	while(scanf("%s", buf) != EOF) {
-		write(streamSocket, buf, sizeof(buf));
-		read(streamSocket, buf, sizeof(buf));
-		printf("Recieved from server: %s\n", buf);
+	pthread_create(&tid, NULL, read_from_server, (void *) &streamSocket);
+	
+	while(fgets(buf, BUFSIZ, stdin) != NULL) {
+		write(streamSocket, buf, BUFSIZ);
 	}
+	
+	printf("Exiting.\n");
 	
 	close(streamSocket);
 	return(0);
 }
+
+void *read_from_server(void *arg) {
+	char buf[BUFSIZ];
+	int chars_read;
+	int stream_socket = *((int *) arg);
+	while((chars_read = read(stream_socket, buf, BUFSIZ)) > 0) {
+		printf(buf);
+	}
+	
+	printf("Disconnected from the server.\n");
+	return NULL;
+}
+		
